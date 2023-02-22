2d358bc55f7488d6cc128aae4673d8db08bfd5b4
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-02-23 02:39:05 +0330

    setting up the backend

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
