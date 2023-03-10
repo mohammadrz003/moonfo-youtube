7fd80b3e58e81bee8a96fa1c45c45691a176890f
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-03-10 10:19:18 +0330

    fixing bugs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -123,18 +123,21 @@ const updateProfilePicture = async (req, res, next) => {
 
     upload(req, res, async function (err) {
       if (err) {
-        const error = new Error("An unknown error occured when uploading " + err.message);
+        const error = new Error(
+          "An unknown error occured when uploading " + err.message
+        );
         next(error);
       } else {
         // every thing went well
         if (req.file) {
-          const updatedUser = await User.findByIdAndUpdate(
-            req.user._id,
-            {
-              avatar: req.file.filename,
-            },
-            { new: true }
-          );
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          if (filename) {
+            fileRemover(filename);
+          }
+          updatedUser.avatar = req.file.filename;
+          await updatedUser.save();
           res.json({
             _id: updatedUser._id,
             avatar: updatedUser.avatar,
--- utils/fileRemover.js
+++ utils/fileRemover.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 const fileRemover = (filename) => {
-  fs.unlink(path.join(__dirname, "../uploads"), function (err) {
+  fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
     if (err && err.code == "ENOENT") {
       // file doesn't exist
       console.log(`File ${filename} doesn't exist, won't remove it.`);
