9362f349c8f3263a8fcd95d30f89215af5b7d5d2
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-04-08 11:37:23 +0330

    added static folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db";
 import {
   errorResponserHandler,
@@ -19,6 +20,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
