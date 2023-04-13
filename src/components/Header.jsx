dcdf465b2c664a1b9a1ede761304e8f3f6502ed7
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-04-13 10:24:22 +0330

    delete profile picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { images } from "../constants";
 import { logout } from "../store/actions/user";
 
 const navItemsInfo = [
-  { name: "Home", type: "link" },
-  { name: "Articles", type: "link" },
-  { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
-  { name: "Pricing", type: "link" },
-  { name: "Faq", type: "link" },
+  { name: "Home", type: "link", href: "/" },
+  { name: "Articles", type: "link", href: "/articles" },
+  {
+    name: "Pages",
+    type: "dropdown",
+    items: [
+      { title: "About us", href: "/about" },
+      { title: "Contact us", href: "/contact" },
+    ],
+  },
+  { name: "Pricing", type: "link", href: "/pricing" },
+  { name: "Faq", type: "link", href: "/faq" },
 ];
 
 const NavItem = ({ item }) => {
@@ -28,9 +35,9 @@ const NavItem = ({ item }) => {
     <li className="relative group">
       {item.type === "link" ? (
         <>
-          <a href="/" className="px-4 py-2">
+          <Link to={item.href} className="px-4 py-2">
             {item.name}
-          </a>
+          </Link>
           <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
             /
           </span>
@@ -51,13 +58,13 @@ const NavItem = ({ item }) => {
           >
             <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
               {item.items.map((page, index) => (
-                <a
+                <Link
                   key={index}
-                  href="/"
+                  to={page.href}
                   className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
                 >
-                  {page}
-                </a>
+                  {page.title}
+                </Link>
               ))}
             </ul>
           </div>
@@ -87,9 +94,9 @@ const Header = () => {
   return (
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
-        <div>
+        <Link to="/">
           <img className="w-16" src={images.Logo} alt="logo" />
-        </div>
+        </Link>
         <div className="lg:hidden z-50">
           {navIsVisible ? (
             <AiOutlineClose
--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -4,21 +4,66 @@ import { HiOutlineCamera } from "react-icons/hi";
 
 import { stables } from "../constants";
 import CropEasy from "./crop/CropEasy";
+import { toast } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProfilePicture } from "../services/index/users";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { userActions } from "../store/reducers/userReducers";
 
 const ProfilePicture = ({ avatar }) => {
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
   const [openCrop, setOpenCrop] = useState(false);
   const [photo, setPhoto] = useState(null);
 
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }) => {
+      return updateProfilePicture({
+        token: token,
+        formData: formData,
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(userActions.setUserInfo(data));
+      setOpenCrop(false);
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile Photo is removed");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    setPhoto({url: URL.createObjectURL(file), file});
+    setPhoto({ url: URL.createObjectURL(file), file });
     setOpenCrop(true);
-  }
+  };
+
+  const handleDeleteImage = () => {
+    if (window.confirm("Do you want to delete your profile picture")) {
+      try {
+        const formData = new FormData();
+        formData.append("profilePicture", undefined);
+
+        mutate({ token: userState.userInfo.token, formData: formData });
+      } catch (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <>
       {openCrop &&
-        createPortal(<CropEasy photo={photo} setOpenCrop={setOpenCrop} />, document.getElementById("portal"))}
+        createPortal(
+          <CropEasy photo={photo} setOpenCrop={setOpenCrop} />,
+          document.getElementById("portal")
+        )}
 
       <div className="w-full flex items-center gap-x-4">
         <div className="relative w-20 h-20 rounded-full outline outline-offset-2 outline-1 lutline-primary overflow-hidden">
@@ -38,9 +83,15 @@ const ProfilePicture = ({ avatar }) => {
               </div>
             )}
           </label>
-          <input type="file" className="sr-only" id="profilePicture" onChange={handleFileChange} />
+          <input
+            type="file"
+            className="sr-only"
+            id="profilePicture"
+            onChange={handleFileChange}
+          />
         </div>
         <button
+          onClick={handleDeleteImage}
           type="button"
           className="border border-red-500 rounded-lg px-4 py-2 text-red-500"
         >
