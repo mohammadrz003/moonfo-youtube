e049b56a6640327f6d9f9eb1e782aec612a3f5d7
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-05-15 12:30:26 +0330

    p40 - delete a comment

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -4,7 +4,11 @@ import { useSelector } from "react-redux";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createNewComment, updateComment } from "../../services/index/comments";
+import {
+  createNewComment,
+  deleteComment,
+  updateComment,
+} from "../../services/index/comments";
 import { toast } from "react-hot-toast";
 
 const CommentsContainer = ({
@@ -47,6 +51,20 @@ const CommentsContainer = ({
     },
   });
 
+  const { mutate: mutateDeleteComment } = useMutation({
+    mutationFn: ({ token, desc, commentId }) => {
+      return deleteComment({ token, commentId });
+    },
+    onSuccess: () => {
+      toast.success("Your comment is deleted successfully");
+      queryClient.invalidateQueries(["blog", postSlug]);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
   const addCommentHandler = (value, parent = null, replyOnUser = null) => {
     mutateNewComment({
       desc: value,
@@ -67,7 +85,9 @@ const CommentsContainer = ({
     setAffectedComment(null);
   };
 
-  const deleteCommentHandler = (commentId) => {};
+  const deleteCommentHandler = (commentId) => {
+    mutateDeleteComment({ token: userState.userInfo.token, commentId });
+  };
 
   return (
     <div className={`${className}`}>
--- src/services/index/comments.js
+++ src/services/index/comments.js
@@ -54,3 +54,20 @@ export const updateComment = async ({ token, desc, commentId }) => {
     throw new Error(error.message);
   }
 };
+
+export const deleteComment = async ({ token, commentId }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.delete(`/api/comments/${commentId}`, config);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
