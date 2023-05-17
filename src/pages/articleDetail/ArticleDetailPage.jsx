442ce60ed54b9e69347122bf8660f5a6a98122ec
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-05-17 23:48:35 +0330

    p41 - making the data of the sidebar of the article detail page dynamic

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -15,47 +15,10 @@ import SocialShareButtons from "../../components/SocialShareButtons";
 import { images, stables } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
 import { useQuery } from "@tanstack/react-query";
-import { getSinglePost } from "../../services/index/posts";
+import { getAllPosts, getSinglePost } from "../../services/index/posts";
 import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
 import ErrorMessage from "../../components/ErrorMessage";
 import { useSelector } from "react-redux";
-
-const postsData = [
-  {
-    _id: "1",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "2",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "3",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "4",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-];
-
-const tagsData = [
-  "Medical",
-  "Lifestyle",
-  "Learn",
-  "Healthy",
-  "Food",
-  "Diet",
-  "Education",
-];
 
 const ArticleDetailPage = () => {
   const { slug } = useParams();
@@ -78,6 +41,11 @@ const ArticleDetailPage = () => {
         )
       );
     },
+  });
+
+  const { data: postsData } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
   });
 
   return (
@@ -124,7 +92,7 @@ const ArticleDetailPage = () => {
             <SuggestedPosts
               header="Latest Article"
               posts={postsData}
-              tags={tagsData}
+              tags={data?.tags}
               className="mt-8 lg:mt-0 lg:max-w-xs"
             />
             <div className="mt-7">
@@ -132,12 +100,8 @@ const ArticleDetailPage = () => {
                 Share on:
               </h2>
               <SocialShareButtons
-                url={encodeURI(
-                  "https://moonfo.com/post/client-side-and-server-side-explanation"
-                )}
-                title={encodeURIComponent(
-                  "Client-side and Server-side explanation"
-                )}
+                url={encodeURI(window.location.href)}
+                title={encodeURIComponent(data?.title)}
               />
             </div>
           </div>
--- src/pages/articleDetail/container/SuggestedPosts.jsx
+++ src/pages/articleDetail/container/SuggestedPosts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { images, stables } from "../../../constants";
 
 const SuggestedPosts = ({ className, header, posts = [], tags }) => {
   return (
@@ -17,12 +18,16 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
           >
             <img
               className="aspect-square object-cover rounded-lg w-1/5"
-              src={item.image}
-              alt="laptop"
+              src={
+                item?.image
+                  ? stables.UPLOAD_FOLDER_BASE_URL + item?.image
+                  : images.samplePostImage
+              }
+              alt={item.title}
             />
             <div className="text-sm font-roboto text-dark-hard font-medium">
               <h3 className="text-sm font-roboto text-dark-hard font-medium md:text-base lg:text-lg">
-                {item.title}
+                <Link to={`/blog/${item.slug}`}>{item.title}</Link>
               </h3>
               <span className="text-xs opacity-60">
                 {new Date(item.createdAt).toLocaleDateString("en-US", {
@@ -38,17 +43,23 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
       <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
         Tags
       </h2>
-      <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
-        {tags.map((item, index) => (
-          <Link
-            key={index}
-            to="/"
-            className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
-          >
-            {item}
-          </Link>
-        ))}
-      </div>
+      {tags.length === 0 ? (
+        <p className="text-slate-500 text-xs mt-2">
+          There is not tags for this post
+        </p>
+      ) : (
+        <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
+          {tags.map((item, index) => (
+            <Link
+              key={index}
+              to="/"
+              className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
+            >
+              {item}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
