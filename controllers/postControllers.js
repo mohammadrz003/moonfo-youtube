2b47d5c080b279db63d314aaf076273a68a0d5a8
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-06-29 21:43:37 +0330

    p43 - Creating dynamic Search & Pagination of the posts

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -145,14 +145,43 @@ const getPost = async (req, res, next) => {
 
 const getAllPosts = async (req, res, next) => {
   try {
-    const posts = await Post.find({}).populate([
-      {
-        path: "user",
-        select: ["avatar", "name", "verified"],
-      },
-    ]);
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.title = { $regex: filter, $options: "i" };
+    }
+    let query = Post.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Post.countDocuments();
+    const pages = Math.ceil(total / pageSize);
 
-    res.json(posts);
+    if (page > pages) {
+      const error = new Error("No page found");
+      return next(error);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+      ])
+      .sort({ updatedAt: "desc" });
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    return res.json(result);
   } catch (error) {
     next(error);
   }
