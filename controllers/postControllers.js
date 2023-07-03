3cf8ff7aaf2fe71440fa001ea720df8b15aed31a
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-07-03 22:43:36 +0330

    p46 - fixed total post count

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -154,7 +154,7 @@ const getAllPosts = async (req, res, next) => {
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * pageSize;
-    const total = await Post.countDocuments();
+    const total = await Post.find(where).countDocuments();
     const pages = Math.ceil(total / pageSize);
 
     if (page > pages) {
