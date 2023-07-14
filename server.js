9833c42aabecc12eda291558649b72b329bfd676
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-07-14 19:04:21 +0330

    p48 - update post's picture

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import connectDB from "./config/db";
+import cors from "cors";
 import {
   errorResponserHandler,
   invalidPathHandler,
@@ -16,6 +17,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
