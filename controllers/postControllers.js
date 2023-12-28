9f3a368d7f1655dacdbf331b1dc6a50fe58ea2d3
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-12-28 17:52:27 +0330

    p55 - comments management (part 1)

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -181,6 +181,10 @@ const getAllPosts = async (req, res, next) => {
           path: "user",
           select: ["avatar", "name", "verified"],
         },
+        {
+          path: "categories",
+          select: ["title"],
+        },
       ])
       .sort({ updatedAt: "desc" });
 
