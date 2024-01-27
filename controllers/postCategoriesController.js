8fdb8f471c062b281579eb19271ac12f593253fc
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2024-01-27 12:42:31 +0330

    p58 - categories creation & management (part 1)

--- controllers/postCategoriesController.js
+++ controllers/postCategoriesController.js
@@ -26,9 +26,36 @@ const createPostCategory = async (req, res, next) => {
 
 const getAllPostCategories = async (req, res, next) => {
   try {
-    const postCategories = await PostCategories.find({});
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.title = { $regex: filter, $options: "i" };
+    }
+    let query = PostCategories.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await PostCategories.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
 
-    return res.json(postCategories);
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    if (page > pages) {
+      return res.json([]);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .sort({ updatedAt: "desc" });
+
+    return res.json(result);
   } catch (error) {
     next(error);
   }
